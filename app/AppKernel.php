dc011f3343651b3375c92cdee3e94e6c76d90f07
amercier8 <a.mercier8@gmail.com>
2018-10-23 19:41:56 +0200

    userbundle created

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new OC\StockManagementBundle\OCStockManagementBundle(),
+            new OC\UserBundle\OCUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
